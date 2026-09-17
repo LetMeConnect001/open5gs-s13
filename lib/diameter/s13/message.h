97827e3d73d25e194f1083fdec8c4e0784563a66
boby <310484878+LetMeConnect001@users.noreply.github.com>
2026-09-17 13:33:58 +0200

    add pei string as stored in eir collection in mongo

--- lib/diameter/s13/message.h
+++ lib/diameter/s13/message.h
@@ -44,6 +44,23 @@ typedef struct ogs_diam_s13_message_s {
     ogs_diam_s13_eca_message_t eca_message;
 } ogs_diam_s13_message_t;
 
+#define OGS_DIAM_S13_IMEI_LEN           14  /* TAC + SNR, no check digit */
+#define OGS_DIAM_S13_SVN_LEN            2
+/* "imeisv-" + 16 digits */
+#define OGS_DIAM_S13_MAX_PEI_LEN \
+    (sizeof(OGS_ID_SUPI_TYPE_IMEISV "-") - 1 + OGS_MAX_IMEISV_BCD_LEN)
+
+/*
+ * PEI string, as stored in the EIR database, out of the Terminal-Information
+ * contents (TS 29.272 7.3.53 and 7.3.54): IMEI (14 digits) + Software-Version
+ * (2 digits) gives "imeisv-<16 digits>", an IMEI alone gives
+ * "imei-<15 digits>" with the Luhn check digit appended.
+ * Returns OGS_ERROR when the input is not usable.
+ */
+int ogs_diam_s13_pei_from_terminal_info(
+        const char *imei, size_t imei_len, const char *svn, size_t svn_len,
+        char *pei, size_t pei_len);
+
 int ogs_diam_s13_init(void);
 
 #ifdef __cplusplus
